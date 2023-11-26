3cc454b630eae755ae2d209f67801eca0f1c8f1d
AdelarnyFEB <abidemiadelani@gmail.com>
2023-11-27 00:52:10 +0100

    Add latest file

--- script.js
+++ script.js
@@ -1,58 +1,116 @@
 const apikey = "79e2468d307d1b90adf244a625bcc2f7";
 
-const weatherDataEl = document.getElementById("weather-data")
+const weatherDataEl = document.getElementById("weather-data");
 
-const inputCityEl = document.getElementById("city-input")
+const countryInputEl = document.getElementById("country-input");
 
 const formEl = document.querySelector("form");
 
+
 formEl.addEventListener("submit", (event) => {
   event.preventDefault();
-  const cityValue = inputCityEl.value;
-  getWeatherData(cityValue);
+  const countryValue = countryInputEl.value;
+  getWeatherData(countryValue);
 });
 
-async function getWeatherData(cityValue) {
-    try {
-      const response = await fetch(
-        `https://api.openweathermap.org/data/2.5/weather?q=${cityValue}&appid=${apikey}&units=metric`
-      );
-  
-      if (!response.ok) {
-        throw new Error("Network response was not ok");
-      }
-  
-      const data = await response.json();
-  
-      const temperature = Math.round(data.main.temp);
-  
-      const description = data.weather[0].description;
-  
-      const icon = data.weather[0].icon;
-  
-      const details = [
-        `Feels like: ${Math.round(data.main.feels_like)}`,
-        `Humidity: ${data.main.humidity}%`,
-        `Wind speed: ${data.wind.speed} m/s`,
-      ];
-  
-      weatherDataEl.querySelector(
-        ".icon"
-      ).innerHTML = `<img src="http://openweathermap.org/img/wn/${icon}.png" alt="Weather Icon">`;
-      weatherDataEl.querySelector(
-        ".temperature"
-      ).textContent = `${temperature}°C`;
-      weatherDataEl.querySelector(".description").textContent = description;
-  
-      weatherDataEl.querySelector(".details").innerHTML = details
-        .map((detail) => `<div>${detail}</div>`)
-        .join("");
-    } catch (error) {
-      weatherDataEl.querySelector(".icon").innerHTML = "";
-      weatherDataEl.querySelector(".temperature").textContent = "";
-      weatherDataEl.querySelector(".description").textContent =
-        "An error happened, please try again later";
-  
-      weatherDataEl.querySelector(".details").innerHTML = "";
+
+async function getWeatherData(countryValue) {
+  try {
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${countryValue}&appid=${apikey}&units=metric`
+    );
+
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
     }
+
+    const data = await response.json();
+
+    const temperature = Math.round(data.main.temp);
+
+    const description = data.weather[0].description;
+
+    const icon = data.weather[0].icon;
+
+    const details = [
+      `Feels like: ${Math.round(data.main.feels_like)}`,
+      `Humidity: ${data.main.humidity}%`,
+      `Wind speed: ${data.wind.speed} m/s`,
+    ];
+
+    weatherDataEl.querySelector(
+      ".icon"
+    ).innerHTML = `<img src="http://openweathermap.org/img/wn/${icon}.png" alt="Weather Icon">`;
+    weatherDataEl.querySelector(
+      ".temperature"
+    ).textContent = `${temperature}°C`;
+    weatherDataEl.querySelector(".description").textContent = description;
+
+    weatherDataEl.querySelector(".details").innerHTML = details
+      .map((detail) => `<div>${detail}</div>`)
+      .join("");
+  } catch (error) {
+    weatherDataEl.querySelector(".icon").innerHTML = "";
+    weatherDataEl.querySelector(".temperature").textContent = "";
+    weatherDataEl.querySelector(".description").textContent =
+      "An error happened, please try again later";
+
+    weatherDataEl.querySelector(".details").innerHTML = "";
   }
+};
+
+formEl.addEventListener("click", () => {
+  let countryName = countryInputEl.value;
+  let finalURL = `https://restcountries.com/v3.1/name/${countryName}?fullText=true`;
+  console.log(finalURL);
+  fetch(finalURL)
+    .then((response) => response.json())
+    .then((data) => {
+      countryFact.innerHTML = `
+        <img src="${data[0].flags.svg}" class="flag-img">
+        <h2>${data[0].name.common}</h2>
+        <div class="wrapper">
+            <div class="data-wrapper">
+                <h4>Capital:</h4>
+                <span>${data[0].capital[0]}</span>
+            </div>
+        </div>
+        <div class="wrapper">
+            <div class="data-wrapper">
+                <h4>Continent:</h4>
+                <span>${data[0].continents[0]}</span>
+            </div>
+        </div>
+         <div class="wrapper">
+            <div class="data-wrapper">
+                <h4>Population:</h4>
+                <span>${data[0].population}</span>
+            </div>
+        </div>
+        <div class="wrapper">
+            <div class="data-wrapper">
+                <h4>Currency:</h4>
+                <span>${
+                  data[0].currencies[Object.keys(data[0].currencies)].name
+                } - ${Object.keys(data[0].currencies)[0]}</span>
+            </div>
+        </div>
+         <div class="wrapper">
+            <div class="data-wrapper">
+                <h4>Common Languages:</h4>
+                <span>${Object.values(data[0].languages)
+                  .toString()
+                  .split(",")
+                  .join(", ")}</span>
+            </div>
+        </div>
+      `;
+    })
+    .catch(() => {
+      if (countryName.length == 0) {
+        countryFact.innerHTML = `<h3>The input field cannot be empty</h3>`;
+      } else {
+        countryFact.innerHTML = `<h3>Please enter a valid country name.</h3>`;
+      }
+    });
+});
